bddfbfa68bdee682b7ef15202665f9eb7aa0e486
Henry <hudson.hm10282627@gmail.com>
2025-10-03 11:12:07 -0300

    add etapa 2- função cadastro de livros

--- main.py
+++ main.py
@@ -13,3 +13,30 @@ CREATE TABLE IF NOT EXISTS livros (
     Disponivel TEXT NOT NULL           
 )
 """)
+
+
+def inserir_livros (Titulo,Autor,Ano):
+    try:
+        conexao = sqlite3.connect("Biblioteca.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+        INSERT INTO livros (Titulo,Autor,Ano,Disponivel)
+        VALUES (?,?,?,?)
+        """,
+        (Titulo,Autor,Ano,"sim")
+        )
+        conexao.commit()
+
+    except Exception as erro:
+        print(f"erro ao inserir o livro {erro}")
+
+    finally:
+        if conexao:
+            conexao.close()
+
+Titulo = input("Digitar o Titulo do livro: ").lower()
+Autor = input("Digitar o Autor do livro: ").lower()
+Ano = int(input("Digite o Ano do livro: "))
+
+inserir_livros(Titulo,Autor,Ano)
